d1424fca37f1c793658bcabef33f729f01710428
polovni <luka.polovic11@gmail.com>
2025-04-02 20:31:44 +0200

    Created classes for coordinates and shape. Also displaying a tetrahedron

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
 
 public class DemoViewer {
     public static void main(String[] args) {
@@ -16,6 +18,33 @@ public class DemoViewer {
         JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
         pane.add(pitchSlider, BorderLayout.EAST);
 
+        // tetrahedron
+        ArrayList<Triangle> tris = new ArrayList<>();
+        tris.add(new Triangle(
+                new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(-100, 100, -100),
+                Color.WHITE)
+        );
+        tris.add(new Triangle(
+                new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(100, -100, -100),
+                Color.RED)
+        );
+        tris.add(new Triangle(
+                new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(100, 100, 100),
+                Color.GREEN)
+        );
+        tris.add(new Triangle(
+                new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(-100, -100, 100),
+                Color.BLUE)
+        );
+
         // panel to display render results
         JPanel renderPanel = new JPanel() {
             public void paintComponent(Graphics g) {
@@ -23,7 +52,16 @@ public class DemoViewer {
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
 
-                // TODO rendering magic
+                g2.translate(getWidth() / 2, getHeight() / 2);
+                g2.setColor(Color.WHITE);
+                for(Triangle t : tris) {
+                    Path2D path = new Path2D.Double();
+                    path.moveTo(t.v1.x, t.v1.y);
+                    path.lineTo(t.v2.x, t.v2.y);
+                    path.lineTo(t.v3.x, t.v3.y);
+                    path.closePath();
+                    g2.draw(path);
+                }
             }
         };
 
---
+++ src/Vertex.java
@@ -0,0 +1,17 @@
+public class Vertex {
+
+    // movement left-right
+    double x;
+
+    // movement up-down
+    double y;
+
+    // depth
+    double z;
+
+    Vertex(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
