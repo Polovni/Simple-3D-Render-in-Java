f659ea024bd288625dc2947189e562f0a8ee95da
polovni <luka.polovic11@gmail.com>
2025-04-07 17:13:37 +0200

    Inflate method so triangle becomes an approximation of a sphere

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Path2D;
 import java.awt.image.BufferedImage;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -20,39 +21,43 @@ public class DemoViewer {
         JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
         pane.add(pitchSlider, BorderLayout.EAST);
 
-        // tetrahedron
-        ArrayList<Triangle> tris = new ArrayList<>();
-        tris.add(new Triangle(
-                new Vertex(100, 100, 100),
-                new Vertex(-100, -100, 100),
-                new Vertex(-100, 100, -100),
-                Color.WHITE)
-        );
-        tris.add(new Triangle(
-                new Vertex(100, 100, 100),
-                new Vertex(-100, -100, 100),
-                new Vertex(100, -100, -100),
-                Color.RED)
-        );
-        tris.add(new Triangle(
-                new Vertex(-100, 100, -100),
-                new Vertex(100, -100, -100),
-                new Vertex(100, 100, 100),
-                Color.GREEN)
-        );
-        tris.add(new Triangle(
-                new Vertex(-100, 100, -100),
-                new Vertex(100, -100, -100),
-                new Vertex(-100, -100, 100),
-                Color.BLUE)
-        );
-
         // panel to display render results
         JPanel renderPanel = new JPanel() {
             public void paintComponent(Graphics g) {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
+
+                // tetrahedron
+                ArrayList<Triangle> tris = new ArrayList<>();
+                tris.add(new Triangle(
+                        new Vertex(100, 100, 100),
+                        new Vertex(-100, -100, 100),
+                        new Vertex(-100, 100, -100),
+                        Color.WHITE)
+                );
+                tris.add(new Triangle(
+                        new Vertex(100, 100, 100),
+                        new Vertex(-100, -100, 100),
+                        new Vertex(100, -100, -100),
+                        Color.RED)
+                );
+                tris.add(new Triangle(
+                        new Vertex(-100, 100, -100),
+                        new Vertex(100, -100, -100),
+                        new Vertex(100, 100, 100),
+                        Color.GREEN)
+                );
+                tris.add(new Triangle(
+                        new Vertex(-100, 100, -100),
+                        new Vertex(100, -100, -100),
+                        new Vertex(-100, -100, 100),
+                        Color.BLUE)
+                );
+
+                for (int i = 0; i < 4; i++) {
+                    tris = inflate(tris);
+                }
 
                 // rotation matrix (for left-right)
                 double heading = Math.toRadians(headingSlider.getValue());
@@ -151,8 +156,7 @@ public class DemoViewer {
         frame.setSize(400, 400);
         frame.setVisible(true);
     }
-
-    static public Color getShade(Color color, double shade) {
+    public static Color getShade(Color color, double shade) {
         double redLinear = Math.pow(color.getRed(), 2.4) * shade;
         double greenLinear = Math.pow(color.getGreen(), 2.4) * shade;
         double blueLinear = Math.pow(color.getBlue(), 2.4) * shade;
@@ -162,5 +166,26 @@ public class DemoViewer {
         int blue = (int) Math.pow(blueLinear, 1/2.4);
 
         return new Color(red, green, blue);
+    }
+    public static ArrayList<Triangle> inflate(ArrayList<Triangle> tris) {
+        ArrayList<Triangle> result = new ArrayList<>();
+        for (Triangle t : tris) {
+            Vertex m1 = new Vertex((t.v1.x + t.v2.x) / 2, (t.v1.y + t.v2.y) / 2, (t.v1.z + t.v2.z) / 2);
+            Vertex m2 = new Vertex((t.v2.x + t.v3.x) / 2, (t.v2.y + t.v3.y) / 2, (t.v2.z + t.v3.z) / 2);
+            Vertex m3 = new Vertex((t.v1.x + t.v3.x) / 2, (t.v1.y + t.v3.y) / 2, (t.v1.z + t.v3.z) / 2);
+            result.add(new Triangle(t.v1, m1, m3, t.color));
+            result.add(new Triangle(t.v2, m1, m2, t.color));
+            result.add(new Triangle(t.v3, m2, m3, t.color));
+            result.add(new Triangle(m1, m2, m3, t.color));
+        }
+        for (Triangle t : result) {
+            for (Vertex v : new Vertex[] { t.v1, t.v2, t.v3 } ) {
+                double l = Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z) / Math.sqrt(30000);
+                v.x /= l;
+                v.y /= l;
+                v.z /= l;
+            }
+        }
+        return result;
     }
 }
